30bd4875ae25b9a99d1fc51afea4402add12da3a
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2024-07-08 20:05:19 +0200

    move cmd handling to cobra

--- cmd/iamrotator/main.go
+++ cmd/iamrotator/main.go
@@ -1,72 +1,114 @@
 package main
 
 import (
-	"os"
-
-	"github.com/rusik69/iamrotator/pkg/args"
 	"github.com/rusik69/iamrotator/pkg/aws"
 	"github.com/rusik69/iamrotator/pkg/config"
+
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	logLevelString := os.Getenv("LOG_LEVEL")
-	if logLevelString == "" {
-		logLevelString = "info"
+	var rootCmd = &cobra.Command{
+		Use:   "iamrotator [createuser, removeuser, createstackset, removestackset] -c <configpath>",
+		Short: "IAM Rotator is a tool to manage AWS IAM users and stack sets",
 	}
-	logLevel, err := logrus.ParseLevel(logLevelString)
-	if err != nil {
-		logrus.Warnf("Invalid log level '%s', defaulting to 'info'", logLevelString)
-		logLevel = logrus.InfoLevel
-	}
-	logrus.SetLevel(logLevel)
 
-	arg := args.Parse()
-	cfg, err := config.Load(arg.ConfigPath)
-	if err != nil {
-		logrus.Panic(err)
-	}
-	logrus.Info("Loaded configuration from", arg.ConfigPath)
-	logrus.Debugf("Config: %v\n", cfg)
+	var configPath string
 
-	awsSess, err := aws.CreateSession(cfg.AWS)
-	if err != nil {
-		logrus.Panic(err)
+	rootCmd.PersistentFlags().StringVarP(&configPath, "config", "c", "", "Path to the configuration file")
+	rootCmd.MarkPersistentFlagRequired("config")
+
+	var createUserCmd = &cobra.Command{
+		Use:   "createuser",
+		Short: "Create a new IAM user",
+		Run: func(cmd *cobra.Command, args []string) {
+			cfg, err := config.Load(configPath)
+			if err != nil {
+				logrus.Panic(err)
+			}
+			awsSess, err := aws.CreateSession(cfg.AWS)
+			if err != nil {
+				logrus.Panic(err)
+			}
+			logrus.Info("Checking AWS account ", cfg.AWS.AccountID)
+			newKeyID, NewKeySecret, err := aws.CheckOrCreateIamUser(awsSess, cfg.AWS)
+			if err != nil {
+				logrus.Panic(err)
+			}
+			logrus.Info("Access Key ID:", newKeyID)
+			logrus.Info("Secret Access Key:", NewKeySecret)
+		},
 	}
-	logrus.Info("Checking AWS account ", cfg.AWS.AccountID)
-	if arg.Action == "createuser" {
-		newKeyID, NewKeySecret, err := aws.CheckOrCreateIamUser(awsSess, cfg.AWS)
-		if err != nil {
-			logrus.Panic(err)
-		}
-		logrus.Info("Access Key ID:", newKeyID)
-		logrus.Info("Secret Access Key:", NewKeySecret)
-	} else if arg.Action == "createstackset" {
-		logrus.Info("Checking or creating stack set")
-		err = aws.CheckOrCreateStackSet(awsSess, cfg.AWS)
-		if err != nil {
-			logrus.Panic(err)
-		}
-	} else if arg.Action == "removeuser" {
-		logrus.Info("Removing IAM user")
-		err = aws.RemoveIamUser(awsSess, cfg.AWS)
-		if err != nil {
-			logrus.Panic(err)
-		}
-		logrus.Info("IAM user removed")
-	} else if arg.Action == "removestackset" {
-		err = aws.EmptyStackSet(awsSess, cfg.AWS)
-		if err != nil {
-			logrus.Panic(err)
-		}
-		logrus.Info("Removing stack set")
-		err = aws.RemoveStackSet(awsSess, cfg.AWS)
-		if err != nil {
-			logrus.Panic(err)
-		}
-		logrus.Info("Stack set removed")
-	} else {
-		logrus.Info("Usage: iamrotator <action> <configpath>")
-		os.Exit(1)
+
+	var createStackSetCmd = &cobra.Command{
+		Use:   "createstackset",
+		Short: "Create a new stack set",
+		Run: func(cmd *cobra.Command, args []string) {
+			cfg, err := config.Load(configPath)
+			if err != nil {
+				logrus.Panic(err)
+			}
+			awsSess, err := aws.CreateSession(cfg.AWS)
+			if err != nil {
+				logrus.Panic(err)
+			}
+			logrus.Info("Checking or creating stack set")
+			err = aws.CheckOrCreateStackSet(awsSess, cfg.AWS)
+			if err != nil {
+				logrus.Panic(err)
+			}
+		},
+	}
+
+	var removeUserCmd = &cobra.Command{
+		Use:   "removeuser",
+		Short: "Remove an IAM user",
+		Run: func(cmd *cobra.Command, args []string) {
+			cfg, err := config.Load(configPath)
+			if err != nil {
+				logrus.Panic(err)
+			}
+			awsSess, err := aws.CreateSession(cfg.AWS)
+			if err != nil {
+				logrus.Panic(err)
+			}
+			logrus.Info("Removing IAM user")
+			err = aws.RemoveIamUser(awsSess, cfg.AWS)
+			if err != nil {
+				logrus.Panic(err)
+			}
+			logrus.Info("IAM user removed")
+		},
+	}
+
+	var removeStackSetCmd = &cobra.Command{
+		Use:   "removestackset",
+		Short: "Remove a stack set",
+		Run: func(cmd *cobra.Command, args []string) {
+			cfg, err := config.Load(configPath)
+			if err != nil {
+				logrus.Panic(err)
+			}
+			awsSess, err := aws.CreateSession(cfg.AWS)
+			if err != nil {
+				logrus.Panic(err)
+			}
+			err = aws.EmptyStackSet(awsSess, cfg.AWS)
+			if err != nil {
+				logrus.Panic(err)
+			}
+			logrus.Info("Removing stack set")
+			err = aws.RemoveStackSet(awsSess, cfg.AWS)
+			if err != nil {
+				logrus.Panic(err)
+			}
+			logrus.Info("Stack set removed")
+		},
+	}
+
+	rootCmd.AddCommand(createUserCmd, createStackSetCmd, removeUserCmd, removeStackSetCmd)
+	if err := rootCmd.Execute(); err != nil {
+		logrus.Panic(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/iam v1.34.1
 	github.com/aws/aws-sdk-go-v2/service/organizations v1.29.1
 	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/cobra v1.8.1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -25,6 +26,8 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.26.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.30.1 // indirect
 	github.com/aws/smithy-go v1.20.3 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
